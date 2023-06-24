5d531542b5e17b1f033a6461f017627b964bb741
Arturo Larios <arallariosqu@ittepic.edu.mx>
2023-06-24 15:14:39 -0700

    fix: align logo company

--- src/core/components/ExperienceJob.tsx
+++ src/core/components/ExperienceJob.tsx
@@ -3,7 +3,7 @@ import { WorkExperience } from 'src/core/interfaces/translations/experience.inte
 export const ExperienceJob = ({ experience, index }: { experience: WorkExperience; index: number; }) => {
     return (
         <div className='flex items-start justify-center flex-row md:px-5 sm:mx-auto relative pt-5'>
-            <div className={ `w-[58px] h-[58px] md:w-[90px] md:h-[90px] absolute left-[15px] md:left-1/2 -ml-[43px] bg-[#343230] rounded-full flex justify-center items-center border-4 border-white ${ experience.isCurrent ? 'pulse' : '' }` }>
+            <div className={ `w-[58px] h-[58px] md:w-[90px] md:h-[90px] absolute left-[16px] md:left-1/2 -ml-[43px] bg-[#343230] rounded-full flex justify-center items-center border-4 border-white ${ experience.isCurrent ? 'pulse' : '' }` }>
                 <img src={experience.urlLogo} alt={experience.name} className='w-[28px] h-[28px] md:w-[60px] md:h-[60px] -mt-1'/>
             </div>
             <div className='text-white text-lg relative pl-14 md:pl-0'>
