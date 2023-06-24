0ce8efbb65f5368a8d3cf509641b706e64e4cd24
Arturo Larios <arallariosqu@ittepic.edu.mx>
2023-06-23 22:44:04 -0700

    update: about me

--- src/pages/sections/Experience.tsx
+++ src/pages/sections/Experience.tsx
@@ -11,7 +11,7 @@ export const Experience = () => {
 
     return (
         <div className='relative mt-[80px] xl:mt-[0px]'>
-            <img src={Wave} alt='wave' />
+            <img src={Wave} alt='wave' className='min-h-[312px]'/>
             <div className='bg-[#141C3A] w-full h-auto pb-[200px]'>
                 <h1 className='text-center text-4xl text-white font-semibold mb-[100px] md:mt-[-100px] xl:mt-[-150px]'>{`${t('experience.title')}`}</h1>
                 <div className="mx-10 md:mx-0 xl:mt-[-50px] before:content-[''] before:h-full before:absolute md:before:left-1/2 before:w-1 before:bg-white pt-10 relative">
